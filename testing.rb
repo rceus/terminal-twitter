1f2ea15e962df31355632763fe18b17c2cca1956
rishabhjain <rishabhjain2795@gmail.com>
2014-07-13 18:07:21 +0000

    removed my twitter account keys

--- testing.rb
+++ testing.rb
@@ -16,10 +16,6 @@ class Terminaltwitter
 		print("Enter Access Token Secret: ")
 		$asecret = gets().chomp
 		#my_keys
-		$ckey='gTfHpoDtJOg61HvT8OA0N8q28'
-		$csecret='a23gfGTukB24pXhjFoo5dHjYHjDfXQaDeN1cmn1dJPyeVvgg4A'
-		$atoken='1266814304-ZPf6Hti2lyi0JErbKZF0x196n894AamPejy7JEP'
-		$asecret='e9K2RQtbZKSxNvKoDugslInRmM4kdmatZUJV7x4uvuKH1'
 	end
 	#display
 	def options
